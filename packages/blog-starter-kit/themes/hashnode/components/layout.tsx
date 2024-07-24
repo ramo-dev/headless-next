1fe558a47a3a649f87c0c2c83a190e895aca778d
ramo-dev <annuar.dev@gmail.com>
2024-07-24 19:35:23 +0300

    changing theme to hashnode

--- packages/blog-starter-kit/themes/hashnode/components/layout.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/layout.tsx
@@ -4,19 +4,19 @@ import { Meta } from './meta';
 import { Scripts } from './scripts';
 
 type Props = {
-	children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 export const Layout = ({ children }: Props) => {
-	return (
-		<>
-			<Meta />
-			<Scripts />
-			<div className="min-h-screen bg-white dark:bg-neutral-950">
-				<main>{children}</main>
-			</div>
-			<Analytics />
-			<Integrations />
-		</>
-	);
+  return (
+    <>
+      <Meta />
+      <Scripts />
+      <div className="min-h-screen bg-white dark:bg-black">
+        <main>{children}</main>
+      </div>
+      <Analytics />
+      <Integrations />
+    </>
+  );
 };
--- packages/blog-starter-kit/themes/hashnode/components/post-page-navbar.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/post-page-navbar.tsx
@@ -16,70 +16,70 @@ import { Button } from './custom-button';
 import PublicationLogo from './publication-logo';
 
 type Props = {
-	publication: Pick<PublicationFragment, 'id' | 'title' | 'links' | 'url' | 'features' | 'isTeam' | 'author' | 'preferences'>;
+  publication: Pick<PublicationFragment, 'id' | 'title' | 'links' | 'url' | 'features' | 'isTeam' | 'author' | 'preferences'>;
 };
 
 const PostPageNavbar = forwardRef<HTMLElement, Props>((props, ref) => {
-	const { publication } = props;
+  const { publication } = props;
 
-	useStickyNavScroll({ elRef: ref });
+  useStickyNavScroll({ elRef: ref });
 
-	const commonIconBtnStyles = getCommonBtnStyles();
+  const commonIconBtnStyles = getCommonBtnStyles();
 
-	return (
-		<div className="container mx-auto px-2 md:px-4 md:py-1 2xl:px-10">
-			<div className="relative z-40 flex flex-row items-center justify-between pb-2 pt-8 md:py-4">
-				<div
-					className={twJoin(
-						'mb-2 flex flex-row items-center md:mb-0','dark:text-white',
-					)}
-				>
-					<HeaderTooltip
-						tooltipClassName="blog-home-tooltip"
-						tooltipText="Home"
-					>
-						<Link
-							href="/"
-							aria-label="Back to blog home"
-							className={twJoin('blog-back-to-home-button', commonIconBtnStyles, 'mr-2 p-3')}
-						>
-							<ChevronLeftSVG className="h-4 w-4 fill-current pr-1" />
-						</Link>
-					</HeaderTooltip>
+  return (
+    <div className="container mx-auto px-2 md:px-4 md:py-1 2xl:px-10">
+      <div className="relative z-40 flex flex-row items-center justify-between pb-2 pt-8 md:py-4">
+        <div
+          className={twJoin(
+            'mb-2 flex flex-row items-center md:mb-0', 'dark:text-white',
+          )}
+        >
+          <HeaderTooltip
+            tooltipClassName="blog-home-tooltip"
+            tooltipText="Home"
+          >
+            <Link
+              href="/"
+              aria-label="Back to blog home"
+              className={twJoin('blog-back-to-home-button', commonIconBtnStyles, 'mr-2 p-3')}
+            >
+              <ChevronLeftSVG className="h-4 w-4 fill-current pr-1" />
+            </Link>
+          </HeaderTooltip>
 
-					{/* Navigation for mobile view */}
-					<div className="mr-2">
-						<HeaderLeftSidebar publication={publication} />
-					</div>
+          {/* Navigation for mobile view */}
+          <div className="mr-2">
+            <HeaderLeftSidebar publication={publication} />
+          </div>
 
-					<div className="hidden md:block">
-						<PublicationLogo publication={publication} size="sm" withProfileImage isPostPage />
-					</div>
-				</div>
+          <div className="hidden md:block">
+            <PublicationLogo publication={publication} size="sm" withProfileImage isPostPage />
+          </div>
+        </div>
 
-				<div
-					className={twJoin(
-						'flex flex-row items-center','dark:text-white',
-					)}
-				>
-					<HeaderBlogSearch publication={publication} />
-					<Button as="a" href="#" type="primary" label="Sign up" />
-				</div>
-			</div>
+        <div
+          className={twJoin(
+            'flex flex-row items-center', 'dark:text-white',
+          )}
+        >
+          <HeaderBlogSearch publication={publication} />
+          <Button as="a" href="#" type="primary" label="Sign up" />
+        </div>
+      </div>
 
-			{/* Logo for mobile view */}
-			<div className="mx-auto my-5 flex w-2/3 flex-row items-center justify-center md:hidden">
-				<PublicationLogo publication={publication} size="xl" isPostPage />
-			</div>
+      {/* Logo for mobile view */}
+      <div className="mx-auto my-5 flex w-2/3 flex-row items-center justify-center md:hidden">
+        <PublicationLogo publication={publication} size="xl" isPostPage />
+      </div>
 
-			<div className="blog-sub-header mb-4 md:hidden" data-testid="blog-sub-header">
-				{/* Social Links for mobile view */}
-				<div className="mt-6">
-					<PublicationSocialLinks links={publication.links} />
-				</div>
-			</div>
-		</div>
-	);
+      <div className="blog-sub-header mb-4 md:hidden" data-testid="blog-sub-header">
+        {/* Social Links for mobile view */}
+        <div className="mt-6">
+          <PublicationSocialLinks links={publication.links} />
+        </div>
+      </div>
+    </div>
+  );
 });
 
 PostPageNavbar.displayName = 'PostPageNavbar';
